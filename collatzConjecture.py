b2d59bbf191fc4aa7acbcb8443fbb5006e9152ec
Norbert Acedanski <norbert.acedanski@gmail.com>
2022-01-02 15:27:56 +0100

    Initial commit

---
+++ collatzConjecture.py
@@ -0,0 +1,46 @@
+import matplotlib.pyplot as plt
+import re
+
+class CollatzConjecture():
+    def __init__(self, startNumber):
+        self.startNumber = startNumber
+        self.collatzNumbers = []
+        self.xValues = []
+
+    def computeCollatzNumbers(self):
+        self.collatzNumbers.append(self.startNumber)
+        number = self.startNumber
+        i = 0
+        self.xValues.append(i)
+        while number != 1:
+            if number % 2 == 0:
+                number = int(number/2)
+            else:
+                number = int(3*number + 1)
+            i += 1
+            self.collatzNumbers.append(number)
+            self.xValues.append(i)
+        print("Collatz numbers for the number " + str(self.startNumber) + ": " + str(self.collatzNumbers))
+        print("Number of steps: " + str(len(self.xValues)))
+        print("Maximum number: " + str(max(self.collatzNumbers)))
+    
+    def plotNumbers(self):
+        fig = plt.figure()
+        fig.clear()
+        plt.plot(self.xValues, self.collatzNumbers)
+        plt.draw()
+        plt.xlabel("Steps")
+        plt.ylabel("Numbers after each iteration")
+        plt.title("Measurements")
+        plt.show()
+
+    def run(self):
+        self.computeCollatzNumbers()
+        self.plotNumbers()
+
+if __name__ == '__main__':
+    print("Program that shows Collatz Conjecture and Hillstone numbers")
+    input = int(input("Provide the number: "))
+    number = re.sub('[!?@#$\[\]<>%^&*()]a-zA-Z', '', input)
+    collatz = CollatzConjecture(number)
+    collatz.run()
