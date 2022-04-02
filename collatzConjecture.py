3728622e2ec75a53282fce91303ef4dff1fbf60c
Norbert Acedanski <norbert.acedanski@gmail.com>
2022-04-02 17:36:37 +0200

    Rename variables and methods to pythonic names

--- collatzConjecture.py
+++ collatzConjecture.py
@@ -2,35 +2,35 @@ import matplotlib.pyplot as plt
 import re
 
 class CollatzConjecture():
-    def __init__(self, startNumber):
-        self.startNumber = startNumber
-        self.collatzNumbers = []
-        self.xValues = []
+    def __init__(self, start_number):
+        self.start_number = start_number
+        self.collatz_numbers = []
+        self.x_values = []
 
-    def computeCollatzNumbers(self):
-        number = self.startNumber
+    def compute_collatz_numbers(self):
+        number = self.start_number
         iteration = 0
-        self.collatzNumbers.append(number)
-        self.xValues.append(iteration)
+        self.collatz_numbers.append(number)
+        self.x_values.append(iteration)
         while (number != 1 and number != -1 and number != -5 and number != -17) or iteration == 0:
             if number % 2 == 0:
                 number = int(number/2)
             else:
                 number = int(3*number + 1)
             iteration += 1
-            self.collatzNumbers.append(number)
-            self.xValues.append(iteration)
-        print("Collatz numbers for the number " + str(self.startNumber) + ": " + str(self.collatzNumbers))
-        print("Number of steps: " + str(len(self.xValues) - 1))
-        if self.collatzNumbers[0] > 0:
-            print("Maximum number: " + str(max(self.collatzNumbers)))
+            self.collatz_numbers.append(number)
+            self.x_values.append(iteration)
+        print("Collatz numbers for the number " + str(self.start_number) + ": " + str(self.collatz_numbers))
+        print("Number of steps: " + str(len(self.x_values) - 1))
+        if self.collatz_numbers[0] > 0:
+            print("Maximum number: " + str(max(self.collatz_numbers)))
         else:
-            print("Maximum number: " + str(min(self.collatzNumbers)))
+            print("Maximum number: " + str(min(self.collatz_numbers)))
     
-    def plotNumbers(self):
+    def plot_numbers(self):
         fig = plt.figure("Colatz Conjecture Graph")
         fig.clear()
-        plt.plot(self.xValues, self.collatzNumbers, marker="o")
+        plt.plot(self.x_values, self.collatz_numbers, marker="o")
         plt.draw()
         plt.xlabel("Steps")
         plt.ylabel("Numbers after each iteration")
@@ -38,17 +38,17 @@ class CollatzConjecture():
         plt.show()
 
     def run(self):
-        self.computeCollatzNumbers()
-        self.plotNumbers()
+        self.compute_collatz_numbers()
+        self.plot_numbers()
 
 if __name__ == '__main__':
     print("Program that shows Collatz Conjecture and Hillstone numbers")
-    numbersString = ""
-    while numbersString == "" or numbersString == "0":
-        inputString = input("Provide the number not equal to 0 and an integer: ")
-        numbersString = re.sub('[!?@#$\[\]<>%^&*()a-zA-Z]', '', inputString)
-        if numbersString == "" or numbersString == "0":
+    numbers_string = ""
+    while numbers_string == "" or numbers_string == "0":
+        input_string = input("Provide a non-zero integer number: ")
+        numbers_string = re.sub('[!?@#$\[\]<>%^&*()a-zA-Z]', '', input_string)
+        if numbers_string == "" or numbers_string == "0":
             print("Input must be a number and not equal to 0!")
-    number = int(numbersString)
+    number = int(numbers_string)
     collatz = CollatzConjecture(number)
     collatz.run()
